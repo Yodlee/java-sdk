dc0a3ebe15ebda9386d70a9234f43d4eff05c312
asaraswathy <ASaraswathy@yodlee.com>
2019-11-07 11:10:02 +0530

    Updating License header of source files

--- yodlee-api-model/src/main/java/com/yodlee/api/model/verifyaccount/response/VerifyAccountResponse.java
+++ yodlee-api-model/src/main/java/com/yodlee/api/model/verifyaccount/response/VerifyAccountResponse.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2019 Yodlee, Inc. All Rights Reserved.
  *
- * Licensed under the MIT License. See License.txt in the project root for license information.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
  */
 package com.yodlee.api.model.verifyaccount.response;
 
--- yodlee-java-sdk/src/main/java/com/yodlee/sdk/client/ProgressRequestBody.java
+++ yodlee-java-sdk/src/main/java/com/yodlee/sdk/client/ProgressRequestBody.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2019 Yodlee, Inc. All Rights Reserved.
  *
- * Licensed under the MIT License. See License.txt in the project root for license information.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
  */
 /*
  * Yodlee Core APIs This file describes the Yodlee Platform APIs, using the swagger notation. You can use this swagger
